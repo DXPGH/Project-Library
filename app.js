45d329ce9c9e6928f2e459815f505fe857ee1586
DXPGH <dxpbusiness@gmail.com>
2022-08-07 20:20:29 -0500

    Added the app.js and implemented the Book Constructor

---
+++ app.js
@@ -0,0 +1,14 @@
+// Book Constructor
+function Book(title, author, pages, readOrNot) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readOrNot = readOrNot;
+    this.info = () => {
+        return title + " by " + author + ", " + pages + " pages, " + readOrNot;
+    }
+}
+
+const theHobbit = new Book('The Hobbit', "J.R.R. Tolkien", "295", "not read yet");
+
+console.log(theHobbit.info());
