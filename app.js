6f1e774f54a8049ef98dfe73dbd9a7d74bb7bbd2
DXPGH <dxpbusiness@gmail.com>
2022-08-08 17:00:57 -0500

    Added html css files

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+let myLibrary = [];
+
 // Book Constructor
 function Book(title, author, pages, readOrNot) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readOrNot = readOrNot;
-    this.info = () => {
-        return title + " by " + author + ", " + pages + " pages, " + readOrNot;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readOrNot = readOrNot;
+  this.info = () => {
+    return title + " by " + author + ", " + pages + " pages, " + readOrNot;
+  };
 }
 
-const theHobbit = new Book('The Hobbit', "J.R.R. Tolkien", "295", "not read yet");
+function addBookToLibrary() {
+  
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
 
 console.log(theHobbit.info());
