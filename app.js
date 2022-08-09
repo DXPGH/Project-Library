c795125fcb759363843855689e815e76b83d5b20
DXPGH <dxpbusiness@gmail.com>
2022-08-09 18:41:46 -0500

    Added the Styling and JS functionality

--- app.js
+++ app.js
@@ -1,20 +1,82 @@
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+const content = document.querySelector(".content");
+
 let myLibrary = [];
+let booksToPrint = [];
 
 // Book Constructor
-function Book(title, author, pages, readOrNot) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.readOrNot = readOrNot;
-  this.info = () => {
-    return title + " by " + author + ", " + pages + " pages, " + readOrNot;
-  };
+
+  if (read == true) {
+    this.read = "Read";
+  } else {
+    this.read = "Not Read";
+  }
 }
 
 function addBookToLibrary() {
-  
+  if (title.value == "" || author.value == "" || pages.value == "") {
+    alert("Empty Input Fields");
+    location.reload();
+    return;
+  }
+
+  let book = new Book(title.value, author.value, pages.value, read.checked);
+  myLibrary.push(book);
+  booksToPrint.push(book);
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
+  printBooks(myLibrary.length);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+function changeReadStatus(value) {
+  if (value.innerHTML == "Read") {
+    value.innerHTML = "Not Read";
+  } else {
+    value.innerHTML = "Read";
+  }
+}
 
-console.log(theHobbit.info());
+function deleteCard(cardIndex) {
+  let card = cardIndex;
+  content.removeChild(cardIndex);
+}
+
+function printBooks(index) {
+  booksToPrint.forEach((element) => {
+    let card = document.createElement('div');
+    card.setAttribute("id", index - 1);
+    if (element.read == "Read") {
+      card.classList.add("card");
+      card.innerHTML = `<h5>${element.title}</h5>
+      <p>Author: ${element.author}</p>
+      <p>Pages: ${element.pages}</p>
+      <div class="read">
+        <button type="button" id="${index}button" class="read-button" onclick="changeReadStatus(document.getElementById('${index}button'))">${element.read}</button>
+      </div>
+      <button type="button" class="remove-button" onclick="deleteCard(document.getElementById('${index - 1}')">Remove</button>
+    </div>`;
+    } else {
+      card.classList.add("card");
+      card.innerHTML = `<h5>${element.title}</h5>
+      <p>Author: ${element.author}</p>
+      <p>Pages: ${element.pages}</p>
+      <div class="read">
+        <button type="button" id="${index}button" class="read-button" onclick="changeReadStatus(document.getElementById('${index}button'))">${element.read}</button>
+      </div>
+      <button type="button" class="remove-button" onclick="deleteCard(document.getElementById('${index - 1}'))">Remove</button>
+    </div>`;
+    }
+    
+    content.append(card);
+    booksToPrint.pop();
+  });
+}
